58215bfab9aa82384264083c404b0940591559de
cranberry <scarletskynned@gmail.com>
2025-08-26 09:26:05 +0000

    tests(WIP): save for later

--- tests/manipulation.c
+++ tests/manipulation.c
@@ -15,3 +15,15 @@ enum greatest_test_res test_array_insertion(void)
     array_destroy(a, NULL);
     PASS();
 }
+enum greatest_test_res test_array_insert_at(void)
+{
+    size_t data = 42;
+    array* a = array_create(0, sizeof(size_t));
+    for (size_t i = 0; i < 13; ++i)
+        array_push_back(a, &i);
+    array_insert_at(a, &data, 0);
+    ASSERT_EQ(42, array_at(a, 0));
+    array_insert_at(a, &data, 5);
+    ASSERT_EQ(42, array_at(a, 5));
+    PASS();
+}
