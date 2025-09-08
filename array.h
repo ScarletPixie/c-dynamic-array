dee7ee7ebf33340e1c450fdc243aa2de54d58cda
akino <scarletskynnned@gmail.com>
2025-09-07 21:08:51 -0300

    chore: organize function order and groups in array.h

--- array.h
+++ array.h
@@ -9,7 +9,7 @@ typedef struct
     size_t size;
     size_t capacity;
     size_t DATA_SIZE;
-} array;
+}   array;
 
 array* array_create(size_t capacity, size_t data_size);
 array* array_filter(array* src, int (*filter)(void*), void (*clone)(void* dst, void* src));
@@ -17,15 +17,14 @@ array* array_copy(array* src, void (*clone)(void* dst, void* src));
 
 void array_destroy(array* a, void (*del)(void*));
 void array_clear(array* a, void (*del)(void*));
-
-void* array_push_back(array *a, void* data);
 void* array_erase_back(array *a, void (*del)(void*));
 void* array_erase_at(array *a, size_t pos, void (*del)(void*));
 
+void* array_push_back(array *a, void* data);
+
 int array_is_empty(array *a);
 
-void* array_at(array *a, size_t pos);
-
 void array_map(array* a, void (*f)(void*));
+void* array_at(array *a, size_t pos);
 
 #endif
